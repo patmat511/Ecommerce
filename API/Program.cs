fbdf9efc0908c1ef2225a719b7a7ba4b58c64efa
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-05-01 15:33:27 +0200

    feat(entity): add Product entity and docker compose file

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,19 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.MapControllers();
 
 app.Run();
