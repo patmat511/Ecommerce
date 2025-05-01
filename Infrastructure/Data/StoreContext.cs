796c34c3db1b5be23afe1ffb3809c04fb130043b
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-05-01 16:09:36 +0200

    feat(config): add Config folder

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using System;
 using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -8,4 +9,10 @@ public class StoreContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
 }
