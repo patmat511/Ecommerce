30f774322b574c3db3fbbd93b3ff78eef9b84593
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-05-02 19:16:45 +0200

    feat(repo): add Interfaces and ProductRepository

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,5 +18,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+
+// Seeding with data using method instead migration
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine($"Problem with seeding database: {ex}");
+}
 
 app.Run();
