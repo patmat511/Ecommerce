584cc314e63df7d8cb61ddaa6664bf839072aee5
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-05-03 19:38:40 +0200

    feat(specification): add specification patter interface and evaluator

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,16 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data;
+
+public class SpecificationEvaluator<T> where T : BaseEntity
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec)
+    {
+        if (spec.Criteria is not null)
+        {
+            query = query.Where(spec.Criteria);
+        }
+    }
+}
