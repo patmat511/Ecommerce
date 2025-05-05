6d00f68e173444cb084bec5e7e10b7c6d79329d1
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-05-05 14:45:56 +0200

    feat(dtos): add logic to dtos

--- API/Dtos/ClaimsPrincipalExtensions.cs
+++ API/Dtos/ClaimsPrincipalExtensions.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Security.Claims;
 
 namespace API.Dtos;
 
 public class ClaimsPrincipalExtensions
 {
-
+    public static string RetrieveEmailFromPrincipal(this ClaimsPrincipal user)
+    {
+        return user?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+    }
 }
--- API/Dtos/LoginDto.cs
+++ API/Dtos/LoginDto.cs
@@ -4,5 +4,6 @@ namespace API.Dtos;
 
 public class LoginDto
 {
-
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
